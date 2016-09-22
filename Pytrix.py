655c6912ac44bf61cca73bf2786be65854508035
Harrison Wilco <hwilco@gmail.com>
2016-09-22 17:52:56 -0400

    Added docstrings, minor style/naming changes

--- Pytrix.py
+++ Pytrix.py
@@ -1,6 +1,7 @@
 class MatrixError(Exception):
 	pass
 
+
 class Matrix(object):
 	def __init__(self, mat=None):
 		if mat is None:
@@ -11,11 +12,15 @@ class Matrix(object):
 			if len(row) != length:
 				same_len = False
 		if not same_len:
-			raise MatrixError, "The number of items in each row must be equal"
+			raise MatrixError("The number of items in each row must be equal")
 		self.__refresh_properties__(mat)
 
 	def __refresh_properties__(self, mat):
-		#Called after any changes to a matrix to refresh its rows, columns, rank, etc.
+		"""
+		:param mat: A list representing the elements of the matrix to be refreshed. Each element is a row
+		:return: None
+		Called after any changes to a matrix to refresh its rows, columns, rank, etc.
+		"""
 		self.rows = [row for row in mat]
 		col_lst = []
 		for col_index in range(len(mat[0])):
@@ -24,8 +29,13 @@ class Matrix(object):
 		self.mat = mat
 		self.rank = (len(self.rows), len(self.cols))
 
-	def get_input(self):
-		#Asks user for inputs through console rather than a hard coded input. Called if an empty matrix object is inititalized
+	@staticmethod
+	def get_input():
+		"""
+		:return: A list used to construct a new Matrix object. Each element is a row
+		Asks user for inputs through console rather than a hard coded input. Called if an empty matrix object is
+		initialized.
+		"""
 		matrix_list = []
 		try:
 			rows = int(raw_input("Enter number of rows (m)... "))
@@ -44,53 +54,77 @@ class Matrix(object):
 			matrix_list.append(parsed_row)
 		return matrix_list
 
-	def __repr__(self): #problem if terms are more than one digit, columns not aligned
+	def __repr__(self):  # problem if terms are more than one digit, columns not aligned
 		matrix = self.mat
 		string = ""
 		for row in matrix:
 			string += " ".join([str(x) for x in row]) + "\n"
 		return string
 
-	def addRow(self, new_row, i = None):
-		#Insert new_row (a list) at index i, defaulting to the end of the matrix
+	def add_row(self, new_row, i=None):
+		"""
+		:param new_row: The row to be insterted. Must be the same length as self.rank[1]
+		:param i: The index to insert at. If none given, defaults to after last row
+		:return: None
+		Insert new_row (a list) at index i, defaulting to the end of the matrix.
+		"""
 		if len(new_row) != self.rank[1]:
-			raise MatrixError, "The new row must be the same length as the other rows"
-		if i == None:
+			raise MatrixError("The new row must be the same length as the other rows")
+		if i is None:
 			i = self.rank[0]
 		self.mat.insert(i, new_row)
 		self.__refresh_properties__(self.mat)
 
-	def addCol(self, new_col, i = None):
-        #Insert new_col (a list) at index i, defaulting to the end of the matrix
+	def add_col(self, new_col, i=None):
+		"""
+		:param new_col: The column to be inserted. Must be the same length as self.rank[0]
+		:param i: The index to insert at. If none given, defaults to after last column
+		:return: None
+		Insert new_col (a list) at index i, defaulting to the end of the matrix.
+		"""
 		if len(new_col) != self.rank[0]:
-			raise MatrixError, "The new column must be the same length as the other columns"
-		if i == None:
+			raise MatrixError("The new column must be the same length as the other columns")
+		if i is None:
 			i = self.rank[1]
-		for index, row in enumerate(self.mat):
-			row.append(new_col[index])
+		for row in self.mat:
+			row.append(new_col[0])
 		self.__refresh_properties__(self.mat)
 
-	def __add__(self, matrixB):
-		#Add two matrices of equal rank element by element
-		if type(matrixB) != Matrix:
-			raise MatrixError, "Addition is only defined between two matrices of equal length"
-		if self.rank != matrixB.rank:
-			raise MatrixError, "The matrices must be of equal rank"
+	def __add__(self, matrix_b):
+		"""
+		:param matrix_b: Matrix to add to self
+		:return: Matrix object, sum of two matrices
+		Add two matrices of equal rank element by element
+		"""
+		if type(matrix_b) != Matrix:
+			raise MatrixError("Addition is only defined between two matrices of equal length")
+		if self.rank != matrix_b.rank:
+			raise MatrixError("The matrices must be of equal rank")
 		new_matrix = Matrix(self.mat)
 		for index, rowA in enumerate(new_matrix.rows):
-			new_matrix.mat[index] = [sum(x) for x in zip(rowA, matrixB.rows[index])]
+			new_matrix.mat[index] = [sum(x) for x in zip(rowA, matrix_b.rows[index])]
 		return Matrix(new_matrix.mat)
 
 	def __mul__(self, left_term):
-		#multiplies left_turn on left of matrix -- If a2 * A multiplies each element in A by 2, B * A performs matrix multiplicaiton with B left of A
+		"""
+		:param left_term: The float, int, or Matrix to the left of self, to be multiplied on left of self
+		:return: Matrix object (or in rare cases int or float) that results from matrix or scalar multiplication
+		Multiplies left_term on left of matrix -- b * A multiplies each element in A by 2, B * A performs matrix
+		multiplication with B left of A.
+		"""
 		print self.mat
 		if type(left_term) != Matrix and type(left_term) != int and type(left_term) != float:
-			raise MatrixError, "Multiplication not defined for Matrix * {}".format(type(left_term))
-		#if type(left_term) == int or type(left_term) == float:
-			#work here
+			raise MatrixError('Multiplication not defined for Matrix * {}'.format(type(left_term)))
+		# if type(left_term) == int or type(left_term) == float:
+		# work here
 		'''else:
 			if self.rank[0] != left_term.rank[1]:
 				raise MatrixError, "Invalid dimensions"'''
 
-	#def _rmul_(self, right_term):
-		#def here for right mult
+	# def _rmul_(self, right_term):
+	# def here for right multiplication
+
+
+matrixA = Matrix([[1, 2, 3], [3, 2, 1], [5, 5, 5]])
+matrixA.add_col([4, 4, 4], 0)
+print matrixA
